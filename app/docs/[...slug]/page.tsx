e507d8851bf80dce438b12cc5b955964d79f87a4
Nathan <146884027+natmfat@users.noreply.github.com>
2025-04-16 14:25:38 -0700

    wip: get headings

--- app/docs/[...slug]/page.tsx
+++ app/docs/[...slug]/page.tsx
@@ -1,5 +1,7 @@
 import { notFound } from "next/navigation";
 import { getSlugs } from "./content";
+import React from "react";
+
 export default async function Page({
   params,
 }: {
@@ -7,8 +9,16 @@ export default async function Page({
 }) {
   const { slug } = await params;
   try {
-    const { default: Post } = await import(`../content/${slug.join("/")}.mdx`);
-    return <Post />;
+    const { default: Post, toc } = await import(
+      `../content/${slug.join("/")}.mdx`
+    );
+
+    return (
+      <>
+        {JSON.stringify(toc)}
+        <Post />
+      </>
+    );
   } catch (e) {
     return notFound();
   }
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,32 @@
 import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import withToc from "@stefanprobst/rehype-extract-toc";
+import withTocExport from "@stefanprobst/rehype-extract-toc/mdx";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["mdx", "ts", "tsx"],
+  redirects() {
+    return [
+      {
+        source: "/docs",
+        destination: "/docs/introduction",
+        permanent: true,
+      },
+    ];
+  },
 };
 
-const withMDX = createMDX({});
+const withMDX = createMDX({
+  options: {
+    rehypePlugins: [
+      rehypeSlug,
+      rehypeAutolinkHeadings,
+      withToc,
+      [withTocExport, { name: "toc" }],
+    ],
+  },
+});
 
 export default withMDX(nextConfig);
